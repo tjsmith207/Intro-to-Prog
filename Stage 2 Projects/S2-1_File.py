a6f38461f75db585e02a20d2693eef9948ccc903
traceysmith <tjsmith207@gmail.com>
2015-12-11 06:26:16 -0500

    Final Code Review Response

--- Stage 2 Projects/S2-1_File.py
+++ Stage 2 Projects/S2-1_File.py
@@ -9,5 +9,12 @@
 
 print 3
 print 1 + 1
+print 52 * 3 + 12 * 9
+print (52 * 3) + (12 * 9)
+print 52 * (3 + 12) * 9
+print 365 * 24 * 60 * 60
 
-# Add your own code and notes here
+
+# Write a Python program that prints out the number of minutes in seven weeks.
+
+print 7 * 7 * 24 * 60
